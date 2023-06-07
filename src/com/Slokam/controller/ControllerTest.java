2ceb1869aea3580d27512bc2d83f426ac1f301fd
teja <dhanatejav@gmail.com>
2023-06-07 15:19:13 +0530

    my eclipse file

---
+++ src/com/Slokam/controller/ControllerTest.java
@@ -0,0 +1,14 @@
+package com.Slokam.controller;
+
+public class ControllerTest 
+{
+	public void Test1()
+	{
+		System.out.println("Test1");
+	}
+  public static void main(String[] args)
+  {
+ ControllerTest  ControllerTest=new ControllerTest();
+ ControllerTest.Test1();
+}
+}
